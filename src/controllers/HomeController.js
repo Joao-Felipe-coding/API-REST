1dc7639fb3562818a5a85c80743c386c427bb00d
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-06-06 15:16:47 -0300

    criando usuários
    nessa aula foi feita a implantação da rota store para criar usuários novos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -3,13 +3,13 @@ import Aluno from "../models/Aluno";
 class HomeController {
 	async index(req, res) {
 		const novoAluno = await Aluno.create({
-			nome: 'Nicoli',
-			sobrenome: 'da Silva Batista',
-			email: 'nicolibatista77@gmail.com',
+			nome: "Nicoli",
+			sobrenome: "da Silva Batista",
+			email: "nicolibatista77@gmail.com",
 			idade: 19,
 			peso: 53,
 			altura: 1.61,
-		}) 
+		});
 		res.json(novoAluno);
 	}
 }
