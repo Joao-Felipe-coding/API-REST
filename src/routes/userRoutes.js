9b90d0aed0f7c15231ebd35e6d6e672e79fbae9f
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-06-06 16:15:40 -0300

    CRUD de usuários
    criando as rotas para CRUD dos dados da base de dados

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,10 @@ import userController from "../controllers/UserController";
 const router = new Router();
 
 router.post("/", userController.store);
+router.get("/", userController.index);
+router.get("/:id", userController.show);
+router.put("/:id", userController.update);
+router.delete("/:id", userController.delete);
 
 export default router;
 
@@ -11,6 +15,6 @@ export default router;
 index -> lista todos os usuários -> GET
 store/create -> cria um novo usuário -> POST
 delete -> apaga um usuário -> DELETE
-show -> mostra um usuário -> POST
+show -> mostra um usuário -> GET
 update -> atualiza um usuário -> PATH ou PUT
 */
